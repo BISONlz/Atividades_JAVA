1bf4bc3b1d0babcf77db89b556edd125e7b1ae3b
BISONlz <fluiz21944@gmail.com>
2025-10-11 16:23:38 -0300

    exercicio numero 2

--- src/exercicios/java/num/number_02.java
+++ src/exercicios/java/num/number_02.java
@@ -1,4 +1,19 @@
-package exercicios.java.num1;
-
+package exercicios.java.num;
+import  java.util.Scanner;
 public class number_02 {
+ // Faça um algoritmo para receber um número qualquer e imprimir na tela se o número é par ou ímpar, positivo ou negativo.
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number a: ");
+        int a = sc.nextInt();
+        System.out.println("Enter number b: ");
+        int b = sc.nextInt();
+        int c = a + b;
+        String result= (c  % 2==0) ? "even" : "odd";
+        String result2= (c  >=0) ? "positive" : "negative";
+        System.out.println("The sum is " + result + " and " + result2);
+        sc.close();
+
+
+    }
 }
