dc027ffbf6998d208ca0d9e5938f3b3b0009b0c5
BISONlz <fluiz21944@gmail.com>
2025-10-11 22:24:46 -0300

    exercicio 3

--- src/exercicios/java/numbers/number_01.java
+++ src/exercicios/java/numbers/number_01.java
@@ -1,21 +1,22 @@
-package exercicios.java.num1;
-import java.sql.SQLOutput;
+package exercicios.java.numbers;
+
 import java.util.Scanner;
+
 public class number_01 {
     static void main(String[] args) {
 // Faça um algoritmo que leia os valores de A, B, C e em seguida imprima na tela a soma entre A e B é mostre se a soma é menor que C.
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter number a: ");
-        int a = scanner.nextInt();
+        int a = sc.nextInt();
         System.out.println("Enter number b: ");
-        int b = scanner.nextInt();
+        int b = sc.nextInt();
         System.out.println("Enter number c: ");
-        int c = scanner.nextInt();
+        int c = sc.nextInt();
 
         String result = (a + b >= c) ? "a + b is greater than c" : (a + b == c) ? "a + b is same as c" : "a + b is not greater than c";
 
-        System.out.println( result );
-        scanner.close();
-}
+        System.out.println(result);
+        sc.close();
+    }
 }
--- src/exercicios/java/numbers/number_02.java
+++ src/exercicios/java/numbers/number_02.java
@@ -1,7 +1,9 @@
-package exercicios.java.num;
-import  java.util.Scanner;
+package exercicios.java.numbers;
+
+import java.util.Scanner;
+
 public class number_02 {
- // Faça um algoritmo para receber um número qualquer e imprimir na tela se o número é par ou ímpar, positivo ou negativo.
+    // Faça um algoritmo para receber um número qualquer e imprimir na tela se o número é par ou ímpar, positivo ou negativo.
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number a: ");
@@ -9,8 +11,8 @@ public class number_02 {
         System.out.println("Enter number b: ");
         int b = sc.nextInt();
         int c = a + b;
-        String result= (c  % 2==0) ? "even" : "odd";
-        String result2= (c  >=0) ? "positive" : "negative";
+        String result = (c % 2 == 0) ? "even" : "odd";
+        String result2 = (c >= 0) ? "positive" : "negative";
         System.out.println("The sum is " + result + " and " + result2);
         sc.close();
 
