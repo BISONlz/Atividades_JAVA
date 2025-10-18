5dacd6e2dd54d6dd39c2edb36b592a0b0356fee1
BISONlz <fluiz21944@gmail.com>
2025-10-17 22:57:16 -0300

    exercicio 4

---
+++ src/exercicios/java/numbers/number_04.java
@@ -0,0 +1,21 @@
+package exercicios.java.numbers;
+import java.util.Scanner;
+
+public class number_04 {
+    static void main() {
+
+     Scanner sc = new Scanner(System.in);
+
+     double salario_minimo = 1293.20;
+        System.out.print(" SS salario: ");
+
+
+
+
+
+
+
+
+
+    }
+}
